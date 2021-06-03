88695dae45559b6d75758d84aaf3218add102ff4
Matheus Silva <escorpion.b8@gmail.com>
2021-06-03 10:24:56 -0300

    Os metodos para usuarios foi criado

--- api/src/models/User.ts
+++ api/src/models/User.ts
@@ -1,4 +1,4 @@
-import { Column, Entity, PrimaryColumn } from "typeorm"
+import { Column, Entity, getRepository, PrimaryColumn } from "typeorm"
 import { v4 as uuid } from 'uuid';
 
 @Entity()
@@ -40,6 +40,67 @@ export class User {
     constructor() {
         if (!this.id) {
             this.id = uuid()
+        }
+    }
+
+    async findAll(){
+        try {
+            const repository = getRepository(User)
+            const users = await repository.find()
+            return users
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async insert(user: User){
+        try {
+            const repository = getRepository(User)
+            const exists = await repository.findOne({
+                where: {
+                    cpf: user.cpf
+                }
+            })
+            if (exists) {
+                throw new Error("User already exists");
+            }
+            await repository.save(user)
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async delete(id: string){
+        try {
+            const repository = getRepository(User)
+            const exists = await repository.findOne({
+                where: {
+                    id
+                }
+            })
+            if (!exists) {
+                throw new Error("The user do not exists");
+            }
+            await repository.remove(exists)
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+
+    async update(user: User){
+        try {
+            const repository = getRepository(User)
+            const exists = await repository.findOne({
+                where: {
+                    id: user.id
+                }
+            })
+            if (!exists) {
+                throw new Error("The user do not exists");
+            }
+            await repository.save(user)
+        } catch (error) {
+            console.log(error.message);
         }
     }
 }
