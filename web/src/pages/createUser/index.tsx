c64772d50d1a64faf974c1f88086779b89b9e268
Matheus Silva <escorpion.b8@gmail.com>
2021-06-05 11:03:46 -0300

    Tela de criacao de usuario foi implementada

---
+++ web/src/pages/createUser/index.tsx
@@ -0,0 +1,147 @@
+import React, { FormEvent, useState } from 'react'
+import { Button, Col, Container, Form } from 'react-bootstrap'
+import history from '../../history'
+import api from '../../services/api'
+
+function CreateUser() {
+
+    const [name, setName] = useState<string>('')
+    const [login, setLogin] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [email, setEmail] = useState<string>('')
+    const [phone, setPhone] = useState<string>('')
+    const [cpf, setCpf] = useState<string>('')
+    const [birthDate, setBirthDate] = useState<string>('')
+    const [nameMother, setNameMother] = useState<string>('')
+
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault()
+        console.log({
+            name,
+            login,
+            password,
+            email,
+            phone,
+            cpf,
+            birthDate,
+            nameMother,
+        });
+        try {
+            const response = await api.post('user', {
+                name,
+                login,
+                password,
+                email,
+                phone,
+                cpf,
+                birthDate,
+                nameMother,
+            })
+            window.alert(response.data)
+            history.push('list-users')
+        } catch (error) {
+            console.log(error.response.data.message);
+            window.alert(error.response.data.message)
+        }
+    }
+
+    return (
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group>
+                    <Form.Label>Nome</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter email"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Login</Form.Label>
+                            <Form.Control
+                                type="login"
+                                placeholder="Enter email"
+                                value={login}
+                                onChange={(e) => setLogin(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Password"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Form.Row>
+                <Form.Group>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control
+                        type="email"
+                        placeholder="Enter email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Group>
+                    <Form.Label>Telefone(s)</Form.Label>
+                    <Form.Control
+                        type="tel"
+                        placeholder="Enter Telefone"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+                </Form.Group>
+                <Form.Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Cpf</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter Cpf"
+                                value={cpf}
+                                onChange={(e) => setCpf(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Data de nascimento</Form.Label>
+                            <Form.Control
+                                type="date"
+                                placeholder="Enter email"
+                                max={new Date().toLocaleDateString()}
+                                value={birthDate}
+                                onChange={(e) => setBirthDate(e.target.value)}
+                            />
+                        </Form.Group>
+                    </Col>
+                </Form.Row>
+                <Form.Group>
+                    <Form.Label>Nome da mãe</Form.Label>
+                    <Form.Control
+                        type="text"
+                        placeholder="Enter email"
+                        value={nameMother}
+                        onChange={(e) => setNameMother(e.target.value)}
+                    />
+                </Form.Group>
+                <Button
+                    variant="primary"
+                    type="submit"
+                >
+                    Cadastrar
+                </Button>
+            </Form>
+        </Container>
+    )
+}
+
+export default CreateUser
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import CustomRouter from './CustomRouter'
+import CreateUser from './pages/createUser'
 import ListUsers from './pages/listUsers'
 import Login from './pages/Login'
 import RecoverPassword from './pages/recoverPassword'
@@ -11,6 +12,7 @@ function Routes() {
             <Route path="/" exact component={Login} />
             <Route path="/recover-password" exact component={RecoverPassword} />
             <CustomRouter path="/list-users" exact component={ListUsers} />
+            <CustomRouter path="/create-user" exact component={CreateUser} />
         </Switch>
     )
 }
